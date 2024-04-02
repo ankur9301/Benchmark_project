48b3a33234689cb7c773eced75446fff06381f34
Ankur Gyawali <114321826+ankur9301@users.noreply.github.com>
2024-04-02 19:52:22 -0400

    Setup of integer_operation_benchmark.cpp with initialization of variables

---
+++ integer_operation_benchmark.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <chrono>
+
+int main() {
+    int32_t integerConstant = 2147483647;
+    double result;
+
+    // Starting time
+    auto startTime = std::chrono::high_resolution_clock::now();
+   
+
+    // Placeholder for further operations...
+
+    std::cout << "32_bit Integer Operation Time = ";
+    // Placeholder for duration calculation and printing...
+    return 0;
+}
